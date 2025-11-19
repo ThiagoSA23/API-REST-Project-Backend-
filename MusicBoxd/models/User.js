b9a024c89cd52933f1f287ca15941e786a31c5b4
davidmartinsperes <davidmartinsperes@gmail.com>
2025-11-18 21:40:53 -0300

    Implementando autenticação JWT do User

---
+++ MusicBoxd/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: [true, 'Nome é obrigatório'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email é obrigatório'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email inválido']
+    },
+    senha: {
+        type: String,
+        required: [true, 'Senha é obrigatória'],
+        minlength: 6,
+        select: false
+    }
+}, {
+    timestamps: true
+});
+
+    
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('senha')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.senha = await bcrypt.hash(this.senha, salt);
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.senha);
+};
+
+module.exports = mongoose.model('User', userSchema);
